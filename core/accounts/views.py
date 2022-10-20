79f63775640162b78b1a4e8940c119d8fe320a55
Almukhan Chingiz <chingizpain@gmail.com>
2022-10-21 05:21:48 +0600

    Было добавлено приложение accounts Создан шаблон для входа в аккаунт, а также была создана форма для входа, в urls зарегистрированы пути

---
+++ core/accounts/views.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
+
+from accounts.forms import LoginForm
+
+
+class LoginView(TemplateView):
+    form = LoginForm
+    template_name = 'login.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form()
+        context = {'form': form}
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST)
+        if not form.is_valid():
+            return redirect('login')
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username,
+                            password=password)
+        if not user:
+            return redirect('login')
+        login(request, user)
+        return redirect('main')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('main')
